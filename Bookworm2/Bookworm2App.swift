68182db4ec0a6024dfba59b6477c53e400631e90
Sean Bain <75031999+SEANYB4@users.noreply.github.com>
2021-12-06 08:52:14 +0000

    Initial Commit

---
+++ Bookworm2/Bookworm2App.swift
@@ -0,0 +1,17 @@
+//
+//  Bookworm2App.swift
+//  Bookworm2
+//
+//  Created by Sean Bain on 06/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Bookworm2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bookworm2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bookworm2
+//
+//  Created by Sean Bain on 06/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
